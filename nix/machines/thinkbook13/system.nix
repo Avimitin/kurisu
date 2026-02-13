c87773128f67df8c9b83b04618cee62ef08c135f
Avimitin <dev@avimit.in>
2026-02-13 21:12:53 +0800

    Create a bare-minimal wayland graphic module

--- nix/machines/thinkbook13/system.nix
+++ nix/machines/thinkbook13/system.nix
@@ -12,6 +12,7 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     ../../nixos_modules/ovpn.nix
+    ../../nixos_modules/basic-env.nix
   ];
 
   # Bootloader.
@@ -113,6 +114,7 @@
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
+  programs.fish.enable = true;
   users.users.sh1marin = {
     isNormalUser = true;
     description = "sh1marin";
@@ -123,49 +125,9 @@
     shell = pkgs.fish;
   };
 
-  programs.fish.enable = true;
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    curl
-    git
-    nautilus
-    firefox
-  ];
-
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    nerd-fonts.fira-code
-  ];
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-    "pipe-operators"
-  ];
+  kurisu.basic-env.enable = true;
   nix.settings.substituters = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-  nix.settings.auto-optimise-store = true;
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      terminal.vt = 1;
-      default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --asterisks --remember --remember-session";
-        user = "greeter";
-      };
-    };
-  };
-
-  programs.niri.enable = true;
 }
---
+++ nix/nixos_modules/basic-env.nix
@@ -0,0 +1,62 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.kurisu.basic-env;
+in
+with lib;
+{
+  imports = [ ];
+
+  options.kurisu.basic-env = {
+    enable = mkEnableOption "Minimal Wayland Environment";
+  };
+
+  config = mkIf cfg.enable {
+    services.greetd = {
+      enable = true;
+      settings = {
+        terminal.vt = 1;
+        default_session = {
+          command = "${pkgs.tuigreet}/bin/tuigreet --asterisks --remember --remember-session";
+          user = "greeter";
+        };
+      };
+    };
+
+    programs.niri.enable = true;
+
+    fonts.packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      nerd-fonts.fira-code
+    ];
+
+    environment.systemPackages = with pkgs; [
+      vim
+      wget
+      curl
+      git
+      nautilus
+      firefox
+    ];
+
+    nix.settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "pipe-operators"
+      ];
+      auto-optimise-store = true;
+    };
+
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+}
