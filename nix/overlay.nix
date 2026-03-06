d6b1dba578d3ecafc49533635139cd5d6c4926ec
Avimitin <dev@avimit.in>
2026-03-06 16:00:15 +0800

    Remove opencode overlay
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -20,18 +20,4 @@ final: prev: {
     rev = "0de003833c3677abd1c80bd3e200a59756b51590";
     hash = "sha256-t5aqLWTqCW6BGhqmJpQ5MtQVEiUo0lktiTwxEJ3w1mE=";
   };
-
-  opencode = prev.opencode.overrideAttrs (oldAttrs: rec {
-    version = "1.2.15";
-    src = final.fetchFromGitHub {
-      owner = "anomalyco";
-      repo = "opencode";
-      tag = "v${version}";
-      hash = "sha256-26MV9TbyAF0KFqZtIHPYu6wqJwf0pNPdW/D3gDQEUlQ=";
-    };
-
-    node_modules = oldAttrs.node_modules.overrideAttrs {
-      outputHash = "sha256-Diu/C8b5eKUn7MRTFBcN5qgJZTp0szg0ECkgEaQZ87Y=";
-    };
-  });
 }
