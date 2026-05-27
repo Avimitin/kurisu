18f2e7f35b1496d349a3fd6796941d7af6019903
Avimitin <dev@avimit.in>
2026-05-27 16:22:51 +0800

    Drop auto gc
    Signed-off-by: Avimitin <dev@avimit.in>

--- modules/hm/tools.nix
+++ modules/hm/tools.nix
@@ -206,11 +206,5 @@ in
       ];
       auto-optimise-store = true;
     };
-
-    nix.gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
   };
 }
